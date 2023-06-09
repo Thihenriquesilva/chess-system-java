76b691fc632a378f6f91ebfda8e5f535346723a2
Thiago H <henriquesilva322@outlook.com>
2023-06-09 14:49:09 -0300

    Projeto criado

---
+++ src/application/Program.java
@@ -0,0 +1,8 @@
+package application;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Olá :)");
+        System.out.println("Nova linha adicionada");
+    }
+}
